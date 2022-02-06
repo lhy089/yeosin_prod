3190a61486b1706ccda90cd98bb58de33c3e1d57
YooGree <dbxorms4712@gmail.com>
2022-02-06 16:15:33 +0900

    원서접수 확인 및 취소 및 자격인증서 발급 프린트 기능추가

---
+++ yeosin/src/main/webapp/js/common/print.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+   
+   // 원서접수 확인 및 취소
+   $("#btn_print").click(function(){
+       const completeParam = makeHtml();
+          reportPrint(completeParam);
+      });
+      
+   // 자격인증서 발급
+   $("#btn_print_certificate").click(function(){
+       const completeParam = makeHtml();
+          reportPrintCertificate(completeParam);
+      });
+});
+
+// 프린트할 영역 Html로 만들기
+function makeHtml(){
+    const obj = {html : ''};
+    let html = document.getElementById("printArea").innerHTML ;  
+    obj.html = html;
+    return obj;
+}
+
+// 원서접수 확인 및 취소 프린트 출력
+function reportPrint(param)
+{
+    const setting = "width=890, height=841";
+    const objWin = window.open('', '', setting);
+    
+    var html ="";
+    html += '<html><head><title>프린트</title>';
+    html += '<style>';
+    
+    html += 'table{width:100%; border-top:3px solid #0074dd; box-sizing:border-box; font-family:sandoll-gothicneo3, sans-serif; margin-bottom:70px;}';
+    html += 'th{padding:28px 0; border-bottom:1px solid #c7c6c6; box-sizing:border-box;}';
+    html += 'td{padding:28px 0; border-bottom:1px solid #c7c6c6; box-sizing:border-box;}';
+    html += 'th{text-align:left; font-size:23px; color:#0074dd; font-weight:600; letter-spacing:-0.5px; border-right:1px solid #c7c6c6;}';
+    html += 'td{font-size:23px; color:#000; padding-left:22px;}';
+    html += 'td input{height:50px; border:1px solid #c7c6c6; box-sizing:border-box; margin-right:9px; font-family:sandoll-gothicneo3, sans-serif; font-size:23px; color:#58585b; letter-spacing:-0.5px; text-indent:12px;}';
+    html += 'td input[type="tel"]{width:206px;}';
+    html += 'td input[type="email"]{width:794px;}';  
+    html += 'td input[type="text"]{width:639px;}';
+    html += 'td input[type="radio"]{width:20px; height:20px;}';
+    html += 'td label{font-size:23px; color:#58585b; letter-spacing:-0.5px;}';
+    
+    html += '</style></head>';
+    html += '<body>';
+    html += param.html;
+    html += '</body></html>';
+    
+    objWin.document.open();
+    objWin.document.write(html);
+    objWin.focus(); 
+    objWin.document.close();
+ 
+    setTimeout(function(){objWin.print();objWin.close();}, 1000);
+}
+
+// 자격인증서 프린트 출력
+function reportPrintCertificate(param)
+{
+    const setting = "width=890, height=841";
+    const objWin = window.open('', '', setting);
+    
+    var html ="";
+    html += '<html><head><title>자격인증서</title>';
+    html += '<style>';
+   
+    html += '@media print { * {-webkit-print-color-adjust:exact;}}';
+    html += '.document{width:100%; height:100%; border:1px solid #707070; box-sizing:border-box; background:url("/www/inc/img/state/bg_certificate.jpg") no-repeat center / contain; padding-top:120px; margin-bottom:70px;}';
+    html += '.docNum{font-family:sandoll-gothicneo3, sans-serif; font-size:5px; line-height:1; color:#000; text-indent:90px; margin-bottom:10px;}';
+    html += 'h3{width:100%; height:100px; font-family:ChosunGs, serif; font-size:45px; line-height:80px; color:#000; text-align:center; display:table;}';
+    html += 'h3 p{width:100%; height:100%; display:table-cell; vertical-align:middle;}';
+    html += 'table{width:75%; margin:0 auto; text-align:left;}';
+    html += 'th{font-family:sandoll-gothicneo3, sans-serif; font-size:20px; padding-bottom:20px; line-height:1.48; letter-spacing:-0.5px; box-sizing:border-box;}';
+    html += 'td{font-family:sandoll-gothicneo3, sans-serif; font-size:20px; padding-bottom:20px; line-height:1.48; letter-spacing:-0.5px; box-sizing:border-box;}';
+    html += 'th{color:#58585b;}';
+    html += '.document table td{color:#000; padding-left:16px; background:url("/www/inc/img/state/element_dot.png") no-repeat left 12px /3px;}';
+    html += '.evidence{height:230px; font-family:ChosunGs, serif; font-size:25px; line-height:1.6; color:#000; text-align:center; margin-bottom:60px; display:table;}';
+    html += '.evidence p{height:100%; display:table-cell; vertical-align:middle;}';
+    html += '.date{font-family:ChosunGs, serif; font-size:18px; line-height:1.25; text-align:center;}';
+    html += '</style></head>';
+    html += '<body>';
+    html += param.html;
+    html += '</body></html>';
+    
+    objWin.document.open();
+    objWin.document.write(html);
+    objWin.focus(); 
+    objWin.document.close();
+ 
+    setTimeout(function(){objWin.print();objWin.close();}, 1000);
+}
--- yeosin/src/main/webapp/www/state/certificate.jsp
+++ yeosin/src/main/webapp/www/state/certificate.jsp
@@ -2,27 +2,29 @@
 <!DOCTYPE html>
 <html>
 <head lang="ko">
-  <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-  <meta charset="utf-8">
-  <title>대출성 상품 판매대리•중개업자 등록 자격인증 평가</title>
-  <meta name="description" content="여신금융협회">
-  <meta name="keywords" content="원서접수, 평가응시현황, 시험안내, 알림마당, 회원정보">
-  <meta name="viewport" content="user-scalable=no,
-   initial-scale=1.0,
-   maximum-scale=1.0,
-   minimum-scale=1.0,
-   width=device-width,
-   height=device-height">
-  <meta property="og:type" content="website">
-  <meta property="og:site_name" content="여신금융협회"/>
-  <!-- <meta property="og:url" content="사이트url"> -->
-  <meta property="og:title" content="대출성 상품 판매대리•중개업자 등록 자격인증 평가">
-  <meta property="og:description" content="대출성 상품 판매대리•중개업자 등록 자격인증 평가">
-  <meta property="og:image" content="/www/inc/img/openGraph.jpg">
-  <link rel="shortcut icon" href="/www/inc/img/favicon.png"/>
-  <link rel="icon" href="/www/inc/img/favicon.png" type="image/x-icon">
-  
-  <link rel="stylesheet" href="/www/inc/css/state.css">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+	<meta charset="utf-8">
+	<title>대출성 상품 판매대리•중개업자 등록 자격인증 평가</title>
+	<meta name="description" content="여신금융협회">
+	<meta name="keywords" content="원서접수, 평가응시현황, 시험안내, 알림마당, 회원정보">
+	<meta name="viewport" content="user-scalable=no,
+	 initial-scale=1.0,
+	 maximum-scale=1.0,
+	 minimum-scale=1.0,
+	 width=device-width,
+	 height=device-height">
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="여신금융협회"/>
+	<!-- <meta property="og:url" content="사이트url"> -->
+	<meta property="og:title" content="대출성 상품 판매대리•중개업자 등록 자격인증 평가">
+	<meta property="og:description" content="대출성 상품 판매대리•중개업자 등록 자격인증 평가">
+	<meta property="og:image" content="/www/inc/img/openGraph.jpg">
+	<link rel="shortcut icon" href="/www/inc/img/favicon.png"/>
+	<link rel="icon" href="/www/inc/img/favicon.png" type="image/x-icon">
+	<link rel="stylesheet" href="/www/inc/css/state.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script type="text/javascript" src="${pageContext.request.contextPath}/js/common/print.js?t=1"></script>
 </head>
 
 <body>
@@ -32,38 +34,40 @@
 <div class="state certificate">
   <div class="contentBox">
     <h1>자격인증서발급</h1>
-    <div class="document">
-      <p class="docNum">제2101100002호</p>
-      <h3><p>자격인증서</p></h3>
-      <table>
-        <colgroup>
-          <col width="14%">
-          <col width="">
-        </colgroup>
-        <tr>
-          <th>성명</th>
-          <td>홍길동</td>
-        </tr>
-        <tr>
-          <th>자격구분</th>
-          <td>대출성 상품 판매대리 · 중개업자 등록<br/>자격인증 평가(대출 · 기타 대출성 상품)</td>
-        </tr>
-        <tr>
-          <th>인증일</th>
-          <td>2021-06-24</td>
-        </tr>
-      </table>
-      <div class="evidence">
-        <p>
-          위 사람은 「금융소비자 보호에 관한 법률」<br/>
-          및 같은 법 시행령, 감독규정에 따라 실시한<br/>
-          대출성 상품 판매대리 · 중개업자 자격인증 평가에<br/>
-          합격하였으므로 이 증서를 드립니다.
-        </p>
-      </div>
-      <p class="date">2021년 06월 23일</p>
+    <div id="printArea">
+	    <div class="document">
+	      <p class="docNum">제2101100002호</p>
+	      <h3><p>자격인증서</p></h3>
+	      <table>
+	        <colgroup>
+	          <col width="14%">
+	          <col width="">
+	        </colgroup>
+	        <tr>
+	          <th>성명</th>
+	          <td>홍길동</td>
+	        </tr>
+	        <tr>
+	          <th>자격구분</th>
+	          <td>대출성 상품 판매대리 · 중개업자 등록<br/>자격인증 평가(대출 · 기타 대출성 상품)</td>
+	        </tr>
+	        <tr>
+	          <th>인증일</th>
+	          <td>2021-06-24</td>
+	        </tr>
+	      </table>
+	      <div class="evidence">
+	        <p>
+	          위 사람은 「금융소비자 보호에 관한 법률」<br/>
+	          및 같은 법 시행령, 감독규정에 따라 실시한<br/>
+	          대출성 상품 판매대리 · 중개업자 자격인증 평가에<br/>
+	          합격하였으므로 이 증서를 드립니다.
+	        </p>
+	      </div>
+	      <p class="date">2021년 06월 23일</p>
+	    </div>
     </div>
-    <a href="#" class="btn_apply">출력하기</a>
+	<a onclick="return false;" class="btn_apply" id="btn_print_certificate">출력하기</a>
   </div>
 </div>
 <%@ include file="/www/common/footer.jsp"%>
